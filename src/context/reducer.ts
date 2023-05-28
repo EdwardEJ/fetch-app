d81088bf0bf832c6468158f2a85495a18b0618be
A <jesus.e.estrada@hotmail.com>
2023-05-28 18:07:32 -0400

    modularized context reducer

--- src/context/reducer.ts
+++ src/context/reducer.ts
@@ -1,25 +1,21 @@
+import { DOG_CONTEXT_ACTIONS } from './helpers/dogContextActions';
+import { toggleSelectedFavorite } from './helpers/toggleSelectedFavorite';
 import { Action, State } from './types';
 
 export const reducer = (state: State, action: Action): State => {
 	switch (action.type) {
-		case 'SET_LOGGED_IN':
+		case DOG_CONTEXT_ACTIONS.SET_LOGGED_IN:
 			return { ...state, isLoggedIn: action.payload };
-		case 'SET_DOG_SEARCH_RESPONSE':
+		case DOG_CONTEXT_ACTIONS.SET_DOG_SEARCH_RESPONSE:
 			return { ...state, dogSearchResponse: action.payload };
-		case 'SET_SELECTED_FAVORITES':
-			if (state.selectedFavorite.includes(action.payload)) {
-				return {
-					...state,
-					selectedFavorite: state.selectedFavorite.filter(
-						(value) => value !== action.payload
-					),
-				};
-			} else {
-				return {
-					...state,
-					selectedFavorite: [...state.selectedFavorite, action.payload],
-				};
-			}
+		case DOG_CONTEXT_ACTIONS.SET_SELECTED_FAVORITES:
+			return {
+				...state,
+				selectedFavorite: toggleSelectedFavorite(
+					state.selectedFavorite,
+					action.payload
+				),
+			};
 		default:
 			return state;
 	}
